363792596f64198aaebf96196835f38c05892934
Mahmoud Saleh <ma7mouudbaky@gmail.com>
2025-11-02 00:14:37 +0300

    fix: update getProductById function to accept req and res parameters

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -18,8 +18,9 @@ export const getAllProducts = async (req, res) => {
   }
 };
 
-export const getProductById = async (id) => {
+export const getProductById = async (req, res) => {
   try {
+    const { id } = req.params;
     const product = await Product.findOne({ id });
     return product;
   } catch (error) {
